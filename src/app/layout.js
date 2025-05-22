53abf406729f5d2c71b180f1d697a3bec3b2f00f
Bugrahancobain <bugrahancoban1@gmail.com>
2025-05-22 16:46:33 +0300

    Add Google Analytics Script

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 
@@ -22,15 +23,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={poppins.variable}>
       <body>
-        {/* Background Video */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HD01BGCR06"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HD01BGCR06');
+          `}
+        </Script>
         <div className="global-video-bg">
           <video autoPlay muted loop playsInline>
             <source src="/videos/cloud.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
-
-        {/* Main Content */}
         <Navbar />
         <div className="global-content-overlay">
           {children}
